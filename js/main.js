3d23a805297712c22788d1a8561c2a79febf13fb
bopboy <39391891+bopboy@users.noreply.github.com>
2022-08-31 23:57:10 +0900

    18 특정 타이밍 스크롤 애니메이션 적용

--- js/main.js
+++ js/main.js
@@ -18,8 +18,11 @@
                 messageD: document.querySelector('#scroll-section-0 .main-message.d'),
             },
             values: {
-                messageA_opacity: [0, 1, { start: 0.1, end: 0.2 }],
-                messageB_opacity: [0, 1, { start: 0.3, end: 0.4 }],
+                messageA_opacity_in: [0, 1, { start: 0.1, end: 0.2 }],
+                messageA_opacity_out: [1, 0, { start: 0.25, end: 0.3 }],
+                messageA_translateY_in: [20, 0, { start: 0.1, end: 0.2 }],
+                messageA_translateY_out: [0, -10, { start: 0.25, end: 0.3 }]
+                // messageB_opacity_in: [0, 1, { start: 0.3, end: 0.4 }],
             }
         },
         {
@@ -70,13 +73,14 @@
         let rv
         const scrollHeight = sceneInfo[currentScene].scrollHeight
         const scrollRatio = currentYOffset / scrollHeight
+
         if (values.length === 3) {
             const partScrollStart = values[2].start * scrollHeight
             const partScrollEnd = values[2].end * scrollHeight
             const partScrollHeight = partScrollEnd - partScrollStart
 
             if (currentYOffset >= partScrollStart && currentYOffset <= partScrollEnd) {
-                rv = (currentYOffset - partScrollStart) / partScrollHeight * (values[1] - values[0])
+                rv = (currentYOffset - partScrollStart) / partScrollHeight * (values[1] - values[0]) + values[0];
             } else if (currentYOffset < partScrollEnd) {
                 rv = values[0]
             } else if (currentYOffset > partScrollEnd) {
@@ -91,12 +95,23 @@
         const objs = sceneInfo[currentScene].objs
         const values = sceneInfo[currentScene].values
         const currentYOffset = yOffset - prevScrollHeight
+        const scrollHeight = sceneInfo[currentScene].scrollHeight
+        const scrollRatio = (yOffset - prevScrollHeight) / scrollHeight
 
         switch (currentScene) {
             case 0:
-                let message_opacity_in = calcValues(values.messageA_opacity, currentYOffset)
-                console.log(message_opacity_in)
-                objs.messageA.style.opacity = message_opacity_in
+                const messageA_opacity_in = calcValues(values.messageA_opacity_in, currentYOffset)
+                const messageA_opacity_out = calcValues(values.messageA_opacity_out, currentYOffset)
+                const messageA_translateY_in = calcValues(values.messageA_translateY_in, currentYOffset)
+                const messageA_translateY_out = calcValues(values.messageA_translateY_out, currentYOffset)
+
+                if (scrollRatio <= 0.22) {
+                    objs.messageA.style.opacity = messageA_opacity_in
+                    objs.messageA.style.transform = `translateY(${messageA_translateY_in}%)`
+                } else {
+                    objs.messageA.style.opacity = messageA_opacity_out
+                    objs.messageA.style.transform = `translateY(${messageA_translateY_out}%)`
+                }
                 break
             case 1:
                 break
