32b09faef4a77d9bc13e60b563810eb1d3ebd16c
bopboy <39391891+bopboy@users.noreply.github.com>
2022-09-04 10:34:13 +0900

    55 버그 수정 3 (리사이즈 대응)

--- js/main.js
+++ js/main.js
@@ -448,11 +448,16 @@
             }
         })
         window.addEventListener('resize', () => {
-            if (window.innerWidth > 900) setLayout()
-            sceneInfo[3].values.rectStartY = 0
+            if (window.innerWidth > 900) {
+                // setLayout()
+                // sceneInfo[3].values.rectStartY = 0
+                window.location.reload()
+            }
         })
         window.addEventListener('orientationchange', () => {
-            setTimeout(setLayout, 500)
+            // setTimeout(setLayout, 500)
+            scrollTo(0, 0)
+            setTimeout(() => { window.location.reload() }, 500)
         })
         document.querySelector('.loading').addEventListener('transitionend', e => {
             document.body.removeChild(e.currentTarget)
